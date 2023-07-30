478710f4bc58ef0c48eb253a28cb6b8554c9d58f
Anna <anna051977@gmail.com>
2023-07-30 22:39:18 +0300

    First commit

---
+++ src/main/java/com/example/spring_project_employees/SpringProjectEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_project_employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectEmployeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectEmployeesApplication.class, args);
+    }
+
+}
